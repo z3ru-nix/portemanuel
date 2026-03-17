1dee7fdfc9f4a0d45d8e6298aee70420819f0af8
Aluno <emanuelleao611@gmail.com>
2026-03-17 18:22:16 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const textElement = document.getElementById('dynamic-text');
+const phrases = ["Desenvolvedor de Back-End", "Analista de Dados", "Linux User"];
+let phraseIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let typeSpeed = 100;
+
+function type() {
+    const currentPhrase = phrases[phraseIndex];
+    
+    if (isDeleting) {
+        textElement.textContent = currentPhrase.substring(0, charIndex - 1);
+        charIndex--;
+        typeSpeed = 50;
+    } else {
+        textElement.textContent = currentPhrase.substring(0, charIndex + 1);
+        charIndex++;
+        typeSpeed = 100;
+    }
+
+ 
+    const cursor = document.createElement('span');
+    cursor.className = 'cursor';
+    textElement.appendChild(cursor);
+
+    if (!isDeleting && charIndex === currentPhrase.length) {
+        isDeleting = true;
+        typeSpeed = 2000; 
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        typeSpeed = 500;
+    }
+
+    setTimeout(type, typeSpeed);
+}
+
+ 
+document.addEventListener('DOMContentLoaded', () => {
+    type();
+});
